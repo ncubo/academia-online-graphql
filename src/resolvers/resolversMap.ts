3583baa9ab1ca09b4c6987788fa484d881d4afb5
Miguel Mayorga <mmayorga@gmail.com>
2021-05-15 17:27:35 -0400

    adding mutation for new course, modify and delete

--- src/resolvers/resolversMap.ts
+++ src/resolvers/resolversMap.ts
@@ -1,9 +1,11 @@
 import { IResolvers } from "@graphql-tools/utils";
+import mutation from "./mutation";
 import query from "./query";
 import type from "./types";
 
 const resolversMap: IResolvers = {
     ...query,
+    ...mutation,
     ...type
 }
 
